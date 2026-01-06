ce0b1a30e6e041581e1fcd01da2ff7daa221b6d8
dickmao <none>
2026-01-06 16:05:16 -0500

    gha

--- emacs.nix
+++ emacs.nix
@@ -138,10 +138,6 @@ stdenv.mkDerivation rec {
                   "${lib.getBin stdenv.cc.bintools}/bin"
                   "${lib.getBin stdenv.cc.bintools.bintools}/bin"
                 ]
-                ++ lib.optionals stdenv.hostPlatform.isDarwin [
-                  # The linker needs to know where to find libSystem on Darwin.
-                  "${apple-sdk.sdkroot}/usr/lib"
-                ]
               )
             )
           );
